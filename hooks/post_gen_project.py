bc128c23f63cd1e178cb3c8622ce6d2e88c79a3b
MSalvaris <msalvaris@users.noreply.github.com>
2018-08-23 11:33:01 +0100

    Add post gen script

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,29 @@
+from getpass import getpass
+
+from dotenv import set_key
+
+DOTENV_PATH='.env'
+
+env_key_dict={
+    "SELECTED_SUBSCRIPTION":"{{cookiecutter.selected_subscription}}",
+    "GROUP_NAME":"{{cookiecutter.group_name}}",
+    "STORAGE_ACCOUNT_NAME":"{{cookiecutter.storage_account_name}}",
+    "LOCATION":"{{cookiecutter.location}}",
+    "FILESHARE_NAME":"{{cookiecutter.fileshare_name}}",
+    "SERVICE_PRINCIPAL_APP_NAME":"{{cookiecutter.service_principal_app_name}}"
+}
+
+def set_service_principal_password():
+    pwd=getpass('Please enter the password to use for your service principal')
+    set_key(DOTENV_PATH, 'SERVICE_PRINCIPAL_PASSWORD', pwd)
+
+
+def main():
+    for key, value in env_key_dict.items():
+        set_key(DOTENV_PATH, key, value)
+    set_service_principal_password()
+
+
+
+if __name__ == "__main__":
+    main()
